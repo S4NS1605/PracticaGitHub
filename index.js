ff85a17c86ed39bb70c74a19e3e78c4fea263ac8
Juan Montiel <juandavidmontiel74@gmail.com>
2026-02-11 11:12:32 -0500

    feat: añade interacción visual y control del botón

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const time = document.getElementById("time");
 
 btn.addEventListener("click", ()=>{
 
+    statusText.textContent = "Estado: Enfocado trabajando...";
+    statusText.style.color = "green";
+
+    btn.textContent = "En progreso";
+    btn.disabled = true;
+
 });
